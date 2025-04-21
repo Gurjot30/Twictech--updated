481caf6c27bd4a34bd98c050cf650b77473f8928
Gurjot30 <Gsingh3_be21@thapar.edu>
2025-04-21 15:27:56 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -301,4 +301,15 @@
       });
     });
   });
+
+  
+  window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 10) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+
 })(jQuery);
