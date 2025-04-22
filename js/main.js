088e2123056cae7a5f4ef5272f15abb604aeacfc
Gurjot30 <Gsingh3_be21@thapar.edu>
2025-04-22 12:17:46 +0530

    navbar

--- js/main.js
+++ js/main.js
@@ -303,13 +303,5 @@
   });
 
   
-  window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 10) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
-
+  
 })(jQuery);
